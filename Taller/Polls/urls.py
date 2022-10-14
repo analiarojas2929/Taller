6783144b4a717ad33a9995553507a489d4ae06ba
analiarojas2900 <analia.rojas@duocuc.cl>
2022-10-14 14:55:17 -0300

    commits

--- Taller/Polls/urls.py
+++ Taller/Polls/urls.py
@@ -4,4 +4,7 @@ from Polls.views import index, Contacto, Marcas, Productos, Reserva
 
 urlpatterns = [
     path('', index, name="index"),
+    path('contacto', Contacto, name="contacto"),
+    path('marcas', Marcas, name="marcas"),
+    path('productos', Productos, name="productos"),
 ]
